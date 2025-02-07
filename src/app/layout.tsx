084b5be64ad23c39d14ba17289c656fab5ac510f
Nimira43 <n.rabone72@aol.com>
2025-02-07 21:10:22 +0000

    Cleaned up the remaining files removing unwanted code and bringing in the Poppins font.

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import './globals.css'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+})
+
+export const metadata: Metadata = {
+  title: 'Visionary',
+  description: 'Portfolio builing app using Next JS 15, TypeScript, Tailwind and Shadcn',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={poppins.className}>
+        {children}
+      </body>
+    </html>
+  )
+}
