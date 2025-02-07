31de5a7c47be7757272a29e13acfbc9bbebb0285
Nimira43 <n.rabone72@aol.com>
2025-02-07 21:27:13 +0000

    Updating root page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-export default function page() {
+function HomePage() {
   return (
     <div>
-      <h1 className='text-4xl logo text-primary p-4 bg-dark'>Visionary</h1>
+      <h1 className='text-4xl logo text-primary p-4 bg-dark'>Visionary</h1>      
     </div>
   )
 }
+
+export default HomePage
+
