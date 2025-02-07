81b1a04b03499f62e23d4493c315cf138601c61a
Nimira43 <n.rabone72@aol.com>
2025-02-07 21:40:48 +0000

    Brought in first Shadcn component - a button.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from '@/components/ui/button'
+
 function HomePage() {
   return (
     <div>
-      <h1 className='text-4xl logo text-primary p-4 bg-dark'>Visionary</h1>      
+      <h1 className='text-4xl logo text-primary p-4 bg-dark'>Visionary</h1>
+      <Button className='m-4'>Login</Button>     
     </div>
-  )
+  )  
 }
 
 export default HomePage
