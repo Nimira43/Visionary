36666334eec176c90fc580c9e607f5f05be9581a
Nimira43 <n.rabone72@aol.com>
2025-02-17 17:26:58 +0000

    Testing .env and .gitignore.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ function HomePage() {
   return (
     <div>
       <h1 className='text-4xl logo text-primary p-4 bg-dark'>Visionary</h1>
-      <Button className='m-4'>Login</Button>     
+      <Button className='m-4'>Login</Button>
+      <p>Top Secret Message!!!!!</p>
+      <p>Testing .env and .gitignore</p>
+      <p>Seems like overkill to do this but it has caught me out before.</p>
     </div>
   )  
 }
