017a1ef85534d978178b2f845facd7901387ef21
Nimira43 <n.rabone72@aol.com>
2025-02-24 23:57:59 +0000

    Creating state variable loading

--- src/components/functional/sign-out-button.tsx
+++ src/components/functional/sign-out-button.tsx
@@ -3,11 +3,13 @@
 import { useAuth } from '@clerk/nextjs'
 import { Button } from '../ui/button'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 function SignOutButton() {
   const { signOut } = useAuth()
+  const [loading, setLoading] = useState(false)
   const router = useRouter()
-  
+
   const onSignOut = async () => {
     try {
       await signOut()
