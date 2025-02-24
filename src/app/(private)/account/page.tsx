4762ffdc890c442a92c00778f90e21363bdc2567
Nimira43 <n.rabone72@aol.com>
2025-02-24 23:14:16 +0000

    Bringing in user data to the screen.

--- src/app/(private)/account/page.tsx
+++ src/app/(private)/account/page.tsx
@@ -1,12 +1,16 @@
 import { currentUser } from '@clerk/nextjs/server'
 
-
 async function AccountPage() {
   const loggedInUser = await currentUser()
   console.log(loggedInUser)
   return (
-    <div>
+    <div className='flex flex-col gap-5 text-dark'>
       <h1>Account Page</h1>
+      <hr />
+      <h2>First Name: {loggedInUser?.firstName}</h2>
+      <h2>Last Name: {loggedInUser?.lastName}</h2>
+      <h2>Email: {loggedInUser?.emailAddresses[0].emailAddress}</h2>
+      <h1></h1>
     </div>
   )
 }
