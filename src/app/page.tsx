92211944c679ad633ffc669ae7773a4123496338
Nimira43 <n.rabone72@aol.com>
2025-02-24 20:56:17 +0000

    Imported SignUp from Clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Button } from '@/components/ui/button'
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from '@/components/ui/sheet'
+import { SignUp } from '@clerk/nextjs'
 import { useState } from 'react'
 
 const menuItems = [
