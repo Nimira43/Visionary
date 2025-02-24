a5e0a92f6a6f4249882cac71505df51ec23b3878
Nimira43 <n.rabone72@aol.com>
2025-02-24 23:40:45 +0000

    Brought in useAuth and created variable signOut

--- src/components/functional/sign-out-button.tsx
+++ src/components/functional/sign-out-button.tsx
@@ -1,9 +1,12 @@
 'use-client'
 
+import { useAuth } from '@clerk/nextjs'
 import { Button } from '../ui/button'
 
 function SignOutButton() {
   return (
+    const { signOut } = useAuth
+
     <div>
       <Button>Logout</Button>
     </div>
