3c60af15441a18455431d00e120b70c1e19eba4a
Nimira43 <n.rabone72@aol.com>
2025-02-24 23:16:16 +0000

    Brought in the User ID.

--- src/app/(private)/account/page.tsx
+++ src/app/(private)/account/page.tsx
@@ -4,9 +4,10 @@ async function AccountPage() {
   const loggedInUser = await currentUser()
   console.log(loggedInUser)
   return (
-    <div className='flex flex-col gap-5 text-dark'>
+    <div className='flex flex-col gap-5 p-5 text-dark'>
       <h1>Account Page</h1>
       <hr />
+      <h2>User ID: {loggedInUser?.id}</h2>
       <h2>First Name: {loggedInUser?.firstName}</h2>
       <h2>Last Name: {loggedInUser?.lastName}</h2>
       <h2>Email: {loggedInUser?.emailAddresses[0].emailAddress}</h2>
