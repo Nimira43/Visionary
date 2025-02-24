422c1d437c96d0b125b26239c0655c226ebfc16b
Nimira43 <n.rabone72@aol.com>
2025-02-24 23:20:23 +0000

    Brought in UserButton which allows us to log out.

--- src/app/(private)/account/page.tsx
+++ src/app/(private)/account/page.tsx
@@ -1,8 +1,9 @@
+import { UserButton } from '@clerk/nextjs'
 import { currentUser } from '@clerk/nextjs/server'
 
 async function AccountPage() {
   const loggedInUser = await currentUser()
-  console.log(loggedInUser)
+ 
   return (
     <div className='flex flex-col gap-5 p-5 text-dark'>
       <h1>Account Page</h1>
@@ -11,7 +12,7 @@ async function AccountPage() {
       <h2>First Name: {loggedInUser?.firstName}</h2>
       <h2>Last Name: {loggedInUser?.lastName}</h2>
       <h2>Email: {loggedInUser?.emailAddresses[0].emailAddress}</h2>
-      <h1></h1>
+      <UserButton />
     </div>
   )
 }
