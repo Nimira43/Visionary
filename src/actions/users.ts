f6d4ce6bb9c2ec15150dea37db9a0f7961e9974f
Nimira43 <n.rabone72@aol.com>
2025-03-12 18:35:34 +0000

    Error handling. Also - processing fetch data.

--- src/actions/users.ts
+++ src/actions/users.ts
@@ -8,7 +8,23 @@ export const saveCurrentUser = async () => {}
 export const getCurrentUser = async () => {
   try {
     const clerkUser = await currentUser()
-    const { data, error } = await supabase.from('user_profiles').select('').eq('clerk_user_id', clerkUser?.id)
+    const { data, error } = await supabase
+      .from('user_profiles')
+      .select('')
+      .eq('clerk_user_id', clerkUser?.id)
+    
+    if (error) {
+      throw new Error('Error fetching user data')
+    }
+
+    const user = data.length > 0 ? data[0] : null
+    if (user) { 
+      return {
+        success: true,
+        data : user
+      }
+    }
+  
   } catch (error) {
 
   }
