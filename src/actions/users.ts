51b2b7dbb8d8e25e29a758f919b1b84cde8668f9
Nimira43 <n.rabone72@aol.com>
2025-03-12 18:20:02 +0000

    Import supabase and currentUser. Starting functions to get and save currentuser

---
+++ src/actions/users.ts
@@ -0,0 +1,8 @@
+'use server'
+
+import supabase from '@/config/supabase-db-config'
+import { currentUser } from '@clerk/nextjs/server'
+
+export const saveCurrentUser = async () => { }
+
+export const getCurrentUser = async () => {}
