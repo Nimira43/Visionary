fe690585c568cde164fdb332faafc8930da9ee26
Nimira43 <n.rabone72@aol.com>
2025-03-12 18:29:09 +0000

    Fetching current user and retrieve data from Supabase in try

--- src/actions/users.ts
+++ src/actions/users.ts
@@ -3,6 +3,13 @@
 import supabase from '@/config/supabase-db-config'
 import { currentUser } from '@clerk/nextjs/server'
 
-export const saveCurrentUser = async () => { }
+export const saveCurrentUser = async () => {}
 
-export const getCurrentUser = async () => {}
+export const getCurrentUser = async () => {
+  try {
+    const clerkUser = await currentUser()
+    const { data, error } = await supabase.from('user_profiles').select('').eq('clerk_user_id', clerkUser?.id)
+  } catch (error) {
+
+  }
+}
