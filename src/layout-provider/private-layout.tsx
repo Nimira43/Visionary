903d2ab31eea488a1212b8fc66a8ca0df006786f
Nimira43 <n.rabone72@aol.com>
2025-03-28 17:10:15 +0000

    Created state variable user

--- src/layout-provider/private-layout.tsx
+++ src/layout-provider/private-layout.tsx
@@ -1,8 +1,10 @@
+import React from 'react'
 import PrivateLayoutHeader from './components/header'
 
 function PrivateLayout({ children }: {
   children: React.ReactNode
 }) {
+  const [user, setUser] = React.useState(null)
   return (
     <div>
       <PrivateLayoutHeader />
