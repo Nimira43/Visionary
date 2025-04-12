60e1d48095f064b90a0e42b0de9eedbfd7c202ec
Nimira43 <n.rabone72@aol.com>
2025-04-12 09:53:17 +0100

    Defining  PrivateLayoutSidebar component that takes in onClose (a function) and openSidebar (a boolean) as props.

--- src/layout-provider/components/sidebar.tsx
+++ src/layout-provider/components/sidebar.tsx
@@ -1,10 +1,12 @@
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from '@/components/ui/sheet'
 
-function PrivateLayoutSidebar() {
-  return (
+function PrivateLayoutSidebar({
+  onClose, openSidebar
+}: { onClose: () => void, openSidebar: boolean }) {
+  return ()
     <Sheet
-      open={openSheet}
-      onOpenChange={setOpenSheet}
+      open={openSidebar}
+      onOpenChange={onClose}
     >
       <SheetContent className='min-w-[500px] flex justify-center items-center bg-primary-light'>
         <SheetHeader>
