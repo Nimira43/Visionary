074bc0a583174af2db18a701753e3f3db3243b4c
Nimira43 <n.rabone72@aol.com>
2025-04-18 21:39:44 +0100

    Cleaning up account/page.tsx

--- src/app/(private)/account/page.tsx
+++ src/app/(private)/account/page.tsx
@@ -1,12 +1,19 @@
-import SignOutButton from '@/components/functional/sign-out-button'
-import { UserButton } from '@clerk/nextjs'
+// import SignOutButton from '@/components/functional/sign-out-button'
+// import { UserButton } from '@clerk/nextjs'
 import { currentUser } from '@clerk/nextjs/server'
 
 async function AccountPage() {
   const loggedInUser = await currentUser()
   
   return (
-    <div className='flex flex-col gap-5 p-5 text-dark'>
+    <div>Account Page</div>
+  )
+}
+
+export default AccountPage
+
+
+{/* <div className='flex flex-col gap-5 p-5 text-dark'>
       <h1 className='uppercase font-medium'>Account Page</h1>
       <h2>User ID: {loggedInUser?.id}</h2>
       <h2>First Name: {loggedInUser?.firstName}</h2>
@@ -14,8 +21,4 @@ async function AccountPage() {
       <h2>Email: {loggedInUser?.emailAddresses[0].emailAddress}</h2>
       <UserButton />
       <SignOutButton />
-    </div>
-  )
-}
-
-export default AccountPage
+    </div> */}
