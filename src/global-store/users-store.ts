651ac281729d7d7fb2074327291d630148d90017
Nimira43 <n.rabone72@aol.com>
2025-04-26 09:32:23 +0100

    Initial state of store

--- src/global-store/users-store.ts
+++ src/global-store/users-store.ts
@@ -1,5 +1,7 @@
+import { IUser } from '@/interfaces'
 import { create } from 'zustand'
 
-const useGlobalStore = create(set) => ({
-  
-})
+const useGlobalStore = create((set) => ({
+  user: null,
+  setUser : (user: IUser) => set({ user })
+}))
