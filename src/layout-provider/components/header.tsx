e03c5764beef6538cf31f876ed6e2f1db95c1384
Nimira43 <n.rabone72@aol.com>
2025-04-26 09:57:31 +0100

    Small update.

--- src/layout-provider/components/header.tsx
+++ src/layout-provider/components/header.tsx
@@ -11,7 +11,7 @@ function PrivateLayoutHeader() {
     <div className='bg-primary-light p-5 flex justify-between items-center'>
       <h1 className='text-primary-dark text-3xl logo'>Visionary</h1>
       <div className='flex gap-5 items-center'>
-        <span className='text-sm text-dark'>{user.name}</span>
+        <span className='text-sm text-dark'>{user?.name}</span>
         <Button
           onClick={() => setOpenSidebar(true)}
         >
