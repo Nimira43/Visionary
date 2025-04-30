68391159428153b6efe0a152e624ccb9626faa6b
Nimira43 <n.rabone72@aol.com>
2025-04-30 14:03:23 +0100

    Tidy up code

--- src/app/(private)/account/page.tsx
+++ src/app/(private)/account/page.tsx
@@ -1,5 +1,3 @@
-// import SignOutButton from '@/components/functional/sign-out-button'
-// import { UserButton } from '@clerk/nextjs'
 import { currentUser } from '@clerk/nextjs/server'
 
 async function AccountPage() {
@@ -11,14 +9,3 @@ async function AccountPage() {
 }
 
 export default AccountPage
-
-
-{/* <div className='flex flex-col gap-5 p-5 text-dark'>
-      <h1 className='uppercase font-medium'>Account Page</h1>
-      <h2>User ID: {loggedInUser?.id}</h2>
-      <h2>First Name: {loggedInUser?.firstName}</h2>
-      <h2>Last Name: {loggedInUser?.lastName}</h2>
-      <h2>Email: {loggedInUser?.emailAddresses[0].emailAddress}</h2>
-      <UserButton />
-      <SignOutButton />
-    </div> */}
