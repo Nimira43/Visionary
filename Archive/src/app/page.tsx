c864f4d7c0992576e7f55018548ad20fab372a3d
Nimira43 <n.rabone72@aol.com>
2025-05-22 01:12:43 +0100

    Project has hit issues with Clerk. Restarting project. Archived old code - will use as much of the old code as possible. Created new Next project.

---
+++ Archive/src/app/page.tsx
@@ -0,0 +1,93 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from '@/components/ui/sheet'
+import { SignUp, SignIn } from '@clerk/nextjs'
+import { useSearchParams } from 'next/navigation'
+import { useState } from 'react'
+
+const menuItems = [
+    {
+      title: 'Home',
+      path: '/'
+    },
+    {
+      title: 'About',
+      path: '/about'
+    },
+    {
+      title: 'Contact',
+      path: '/contact'
+    },
+  ]
+
+function HomePage() {
+  const [openSheet, setOpenSheet] = useState(false)
+  const searchParams = useSearchParams()
+  const formType = searchParams.get('formType')
+
+  return (
+    <div className='flex flex-col min-h-screen'>
+      <div className='flex justify-between items-center bg-primary-light px-20 py-5'>
+        <h1 className='text-primary-dark logo text-3xl'>Visionary</h1>
+        <div className='flex justify-end gap-5 items-center'>
+          {menuItems.map((item) => (
+            <span
+              key={item.title}
+              className='text-sm font-light text-primary-dark hover:text-dark cursor-pointer'
+            >
+              {item.title}
+            </span>
+          ))}
+          <Button
+            onClick={() => setOpenSheet(true)}
+            className='font-light uppercase'
+          >
+            Login
+          </Button>
+        </div>
+      </div>
+      <div className='grid grid-cols-2 gap-10 mt-20 h-[70vh] px-20 items-center'>
+        <div className='flex flex-col justify-center'>
+          <h1 className='text-4xl text-primary-medium logo pb-5'>Visualise Your Future</h1>
+          <p className='text-grey-dark font-light'>Unlock your potential with our cutting-edge portfolio builder. Showcase your skills, achievements, and projects in a visually stunning and professional manner. Take control of your career and make your mark with ease. Visualise Your Future today.</p>
+        </div>
+        <div className='flex justify-center'>
+          <img src='/images/hero.png' alt='Hero image' />
+        </div>
+      </div>
+
+      {openSheet && (
+        <Sheet
+          open={openSheet}
+          onOpenChange={setOpenSheet}
+        >
+          <SheetContent className='min-w-[500px] flex justify-center items-center bg-primary-light'>
+            <SheetHeader>
+              <SheetTitle></SheetTitle>
+            </SheetHeader>
+
+            {formType === 'sign-in' ? (
+              <SignIn
+                routing='hash'
+                signUpUrl='/?formType=sign-up'
+                fallbackRedirectUrl='/account'
+              />
+            ) : (
+              <SignUp
+                routing='hash'
+                signInUrl='/?formType=sign-in'
+                fallbackRedirectUrl='/account'  
+              />
+            )}
+ 
+          </SheetContent>
+        </Sheet>
+      )}
+    </div>
+  )
+}
+
+export default HomePage
+
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,93 +1,95 @@
-'use client'
-import { Button } from '@/components/ui/button'
-import { Sheet, SheetContent, SheetHeader, SheetTitle } from '@/components/ui/sheet'
-import { SignUp, SignIn } from '@clerk/nextjs'
-import { useSearchParams } from 'next/navigation'
-import { useState } from 'react'
+import Image from "next/image";
+import styles from "./page.module.css";
 
-const menuItems = [
-    {
-      title: 'Home',
-      path: '/'
-    },
-    {
-      title: 'About',
-      path: '/about'
-    },
-    {
-      title: 'Contact',
-      path: '/contact'
-    },
-  ]
-
-function HomePage() {
-  const [openSheet, setOpenSheet] = useState(false)
-  const searchParams = useSearchParams()
-  const formType = searchParams.get('formType')
-
+export default function Home() {
   return (
-    <div className='flex flex-col min-h-screen'>
-      <div className='flex justify-between items-center bg-primary-light px-20 py-5'>
-        <h1 className='text-primary-dark logo text-3xl'>Visionary</h1>
-        <div className='flex justify-end gap-5 items-center'>
-          {menuItems.map((item) => (
-            <span
-              key={item.title}
-              className='text-sm font-light text-primary-dark hover:text-dark cursor-pointer'
-            >
-              {item.title}
-            </span>
-          ))}
-          <Button
-            onClick={() => setOpenSheet(true)}
-            className='font-light uppercase'
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="https://nextjs.org/icons/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol>
+          <li>
+            Get started by editing <code>src/app/page.tsx</code>.
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
+
+        <div className={styles.ctas}>
+          <a
+            className={styles.primary}
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            Login
-          </Button>
+            <Image
+              className={styles.logo}
+              src="https://nextjs.org/icons/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.secondary}
+          >
+            Read our docs
+          </a>
         </div>
-      </div>
-      <div className='grid grid-cols-2 gap-10 mt-20 h-[70vh] px-20 items-center'>
-        <div className='flex flex-col justify-center'>
-          <h1 className='text-4xl text-primary-medium logo pb-5'>Visualise Your Future</h1>
-          <p className='text-grey-dark font-light'>Unlock your potential with our cutting-edge portfolio builder. Showcase your skills, achievements, and projects in a visually stunning and professional manner. Take control of your career and make your mark with ease. Visualise Your Future today.</p>
-        </div>
-        <div className='flex justify-center'>
-          <img src='/images/hero.png' alt='Hero image' />
-        </div>
-      </div>
-
-      {openSheet && (
-        <Sheet
-          open={openSheet}
-          onOpenChange={setOpenSheet}
+      </main>
+      <footer className={styles.footer}>
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          <SheetContent className='min-w-[500px] flex justify-center items-center bg-primary-light'>
-            <SheetHeader>
-              <SheetTitle></SheetTitle>
-            </SheetHeader>
-
-            {formType === 'sign-in' ? (
-              <SignIn
-                routing='hash'
-                signUpUrl='/?formType=sign-up'
-                fallbackRedirectUrl='/account'
-              />
-            ) : (
-              <SignUp
-                routing='hash'
-                signInUrl='/?formType=sign-in'
-                fallbackRedirectUrl='/account'  
-              />
-            )}
- 
-          </SheetContent>
-        </Sheet>
-      )}
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
     </div>
-  )
+  );
 }
-
-export default HomePage
-
-
-
