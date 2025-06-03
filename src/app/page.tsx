f7b2368494ef2ac8ea50f26aa753ede895756633
Nimira43 <n.rabone72@aol.com>
2025-06-04 00:06:50 +0100

    Testing Shadcn and Tailwind configurations - all good.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 function HomePage() {
   return (
-    <div>
-      <h1>Visionary</h1>
-    </div>
+    <>
+      <div className='bg-dark p-10'>
+        <h1 className='logo text-main text-4xl'>Visionary</h1>
+      </div>
+      <div className='p-10 mt-10'>
+        <Button className='uppercase'>Login</Button>
+      </div>
+    </>
+    
   )
 }
 
