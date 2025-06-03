2bded90cbe1ec787741c78463e7b32dd40982137
Nimira43 <n.rabone72@aol.com>
2025-06-03 23:56:57 +0100

    Set up colours in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,18 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main': '#008080',
+        'support': '#ff4500',
+        'light:': '#fffcfa',
+        'dark': '#111',
+        'grey-light-extra': '#eee',
+        'grey-light': '#ccc',
+        'grey-medium': '#898989',
+        'grey-dark': '#333',
+      }
+    },
   },
   plugins: [],
 }
