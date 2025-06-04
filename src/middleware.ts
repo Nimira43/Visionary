e4af95ca30e1fa601fe2c000f7f1ec813a9de90d
Nimira43 <n.rabone72@aol.com>
2025-06-04 04:28:33 +0100

    Protecting the account route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,10 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+const isPrivate = createRouteMatcher(['/account(.*)'])
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isPrivate(req)) await auth.protect()
+})
 
 export const config = {
   matcher: [
