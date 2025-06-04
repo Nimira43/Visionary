5aec771b6b94bc260e0fbfb6312b1271e571831a
Nimira43 <n.rabone72@aol.com>
2025-06-04 03:39:26 +0100

    Sheetiing up sidebar sheet. Brought in SignIn and SignUp components from Clerk and useSearchParams from Next Navigation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@
 
 import { Button } from '@/components/ui/button'
 import { useState } from 'react'
-import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from '@/components/ui/sheet'
+import { useSearchParams } from 'next/navigation'
+import { SignIn, SignUp } from '@clerk/nextjs'
 
 const menuItems = [
   {
@@ -21,6 +23,8 @@ const menuItems = [
 
 function HomePage() {
   const [openSheet, setOpenSheet] = useState(false) 
+  const searchParams = useSearchParams()
+  const formType = searchParams.get('formType')
 
   return (
     <div className='flex flex-col min-h-screen'>
@@ -28,12 +32,12 @@ function HomePage() {
         <h1 className='logo text-main-dark text-4xl'>Visionary</h1>
         <div className='flex justify-end gap-5 items-center'>
           {menuItems.map((item) => (
-              <span
-                key={item.title}
-                className='text-sm text-dark hover:text-main-dark cursor-pointer uppercase font-normal'
-              >
-                {item.title}
-              </span>
+            <span
+              key={item.title}
+              className='text-sm text-dark hover:text-main-dark cursor-pointer uppercase font-normal'
+            >
+              {item.title}
+            </span>
           ))}
           <Button 
             onClick={() => setOpenSheet(true)}
@@ -44,17 +48,44 @@ function HomePage() {
         </div>
       </div>
       <div className='grid grid-cols-2 gap-10 mt-20 h-[70vh] px-20 '>
-          <div className='flex flex-col justify-center'>
-            <div>
-              <h1 className='text-4xl text-main-dark logo pb-5'>Visionary Solutions</h1>
-              <p className='text-grey-dark mt-2 text-sm font-normal'>Unlock your potential with our cutting-edge portfolio builder. Showcase your skills, achievements, and projects in a visually stunning and professional manner. Take control of your career and make your mark with ease. Visualise Your Future today.</p>
-            </div>
+        <div className='flex flex-col justify-center'>
+          <div>
+            <h1 className='text-4xl text-main-dark logo pb-5'>Visionary Solutions</h1>
+            <p className='text-grey-dark mt-2 text-sm font-normal'>Unlock your potential with our cutting-edge portfolio builder. Showcase your skills, achievements, and projects in a visually stunning and professional manner. Take control of your career and make your mark with ease. Visualise Your Future today.</p>
           </div>
-          <div className='flex justify-center'>
-            <img src="/images/homepage.svg" alt="Home page image" />
-          </div>
-      </div>
-    </div>     
+        </div>
+        <div className='flex justify-center'>
+          <img src="/images/homepage.svg" alt="Home page image" />
+        </div>
+      </div> 
+      {openSheet && (
+        <Sheet
+          open={openSheet}
+          onOpenChange={setOpenSheet}
+        >
+          <SheetContent className='min-w-[500px] flex justify-center items-center bg-primary-light'>
+            <SheetHeader>
+              <SheetTitle></SheetTitle>
+            </SheetHeader>
+
+            {formType === 'sign-in' ? (
+              <SignIn
+                routing='hash'
+                signUpUrl='/?formType=sign-up'
+                fallbackRedirectUrl='/account'
+              />
+            ) : (
+              <SignUp
+                routing='hash'
+                signInUrl='/?formType=sign-in'
+                fallbackRedirectUrl='/account'  
+              />
+            )}
+ 
+          </SheetContent>
+        </Sheet>
+      )}
+    </div>    
   )
 }
 
