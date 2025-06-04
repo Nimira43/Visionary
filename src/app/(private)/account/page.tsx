781249d0558f85c48107ddfc86317a7651a86cc9
Nimira43 <n.rabone72@aol.com>
2025-06-04 04:10:15 +0100

    Bringing in user details to the Account Page.

--- src/app/(private)/account/page.tsx
+++ src/app/(private)/account/page.tsx
@@ -1,7 +1,15 @@
-function AccountPage() {
+import { currentUser } from '@clerk/nextjs/server'
+
+async function AccountPage() {
+  const loggedInUser = await currentUser()
+
   return (
-    <div>
-      AccountPage
+    <div className='flex flex-col gap-5'>
+      <h1>Account Page</h1>
+      <hr />
+      <h1>First Name: {loggedInUser?.firstName}</h1>
+      <h1>Last Name: {loggedInUser?.lastName}</h1>
+      <h1>Email: {loggedInUser?.emailAddresses[0].emailAddress}</h1>
     </div>
   )
 }
