1d8f92fbbf29d55fbc4b1c475010257932954c58
Nimira43 <n.rabone72@aol.com>
2025-06-04 04:12:57 +0100

    Brought in UserButton

--- src/app/(private)/account/page.tsx
+++ src/app/(private)/account/page.tsx
@@ -1,15 +1,18 @@
+import { UserButton } from '@clerk/nextjs'
 import { currentUser } from '@clerk/nextjs/server'
 
 async function AccountPage() {
   const loggedInUser = await currentUser()
 
   return (
-    <div className='flex flex-col gap-5'>
+    <div className='flex flex-col gap-5 p-5'>
       <h1>Account Page</h1>
       <hr />
+      <h1>Clerk User ID: {loggedInUser?.id}</h1>
       <h1>First Name: {loggedInUser?.firstName}</h1>
       <h1>Last Name: {loggedInUser?.lastName}</h1>
       <h1>Email: {loggedInUser?.emailAddresses[0].emailAddress}</h1>
+      <UserButton />
     </div>
   )
 }
