9c7d57976f6631c5de495eeb5eee6bd49068b713
Nimira43 <n.rabone72@aol.com>
2025-07-07 00:32:12 +0100

    Setting account route to PrivateLayout

--- src/layout-provider/index.tsx
+++ src/layout-provider/index.tsx
@@ -1,10 +1,21 @@
 'use client'
 
 import { usePathname } from 'next/navigation'
+import PrivateLayout from './private-layout'
 
 function LayoutProvider({children} : {
   children : React.ReactNode
 }) {
+  const pathname = usePathname()
+
+  if (pathname.startsWith('/account')) {
+    return (
+      <PrivateLayout>
+        {children}
+      </PrivateLayout>
+    )
+  }
+
   return (
     <div>
       
