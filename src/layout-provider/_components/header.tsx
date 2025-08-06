38b3215da603cd48cf81dff210c92240a94102c6
Nimira43 <n.rabone72@aol.com>
2025-08-06 14:07:34 +0100

    Added special Button variant

--- src/layout-provider/_components/header.tsx
+++ src/layout-provider/_components/header.tsx
@@ -1,7 +1,21 @@
+import { Button } from '@/components/ui/button'
+import { TbAlignCenter } from 'react-icons/tb'
+
 function PrivateLayoutHeader() {
   return (
     <div className='bg-main-light p-5 flex justify-between items-center'>
       <h1 className='text-main-dark text-4xl logo'>Visionary</h1>
+      <div className='flex gap-5 items-center'>
+        <span className='text-sm text-main-dark'>Username</span>
+        <Button
+          variant='special'
+        >
+          <TbAlignCenter              
+          size={15}
+        />
+        </Button>
+        
+      </div>
     </div>
   )
 }
