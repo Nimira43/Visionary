f9c442c875cc59303ab04ad310581a6f296019b1
Nimira43 <n.rabone72@aol.com>
2025-08-13 23:21:50 +0100

    Copied over the existing Sheet markup from HomePage to be adapted.

--- src/layout-provider/_components/sidebar.tsx
+++ src/layout-provider/_components/sidebar.tsx
@@ -4,7 +4,31 @@ function PrivateLayoutSidebar({ onClose } : {
   onClose: () => void
 }) {
   return (
-    <div>PrivateLayoutSidebar</div>
+    <Sheet
+      open={openSheet}
+      onOpenChange={setOpenSheet}
+    >
+      <SheetContent className='min-w-[500px] flex justify-center items-center bg-main-light'>
+        <SheetHeader>
+          <SheetTitle></SheetTitle>
+        </SheetHeader>
+
+        {formType === 'sign-in' ? (
+          <SignIn
+            routing='hash'
+            signUpUrl='/?formType=sign-up'
+            fallbackRedirectUrl='/account'
+          />
+        ) : (
+          <SignUp
+            routing='hash'
+            signInUrl='/?formType=sign-in'
+            fallbackRedirectUrl='/account'  
+          />
+        )}
+
+      </SheetContent>
+    </Sheet>
   )
 }
 
