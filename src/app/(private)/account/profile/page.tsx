1506ee6344e39857cb82dc10ec674706826d66b8
Nimira43 <n.rabone72@aol.com>
2025-09-05 04:04:18 +0100

    Import Form and Button

--- src/app/(private)/account/profile/page.tsx
+++ src/app/(private)/account/profile/page.tsx
@@ -3,6 +3,8 @@
 import {zodResolver} from '@hookform/resolvers/zod'
 import {useForm} from 'react-hook-form'
 import { z } from 'zod'
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 
 function ProfilePage() {
   return (
