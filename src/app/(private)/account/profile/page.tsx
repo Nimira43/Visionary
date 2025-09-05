dbaf39b5baf40c6452ccd4e538cbe8448197887e
Nimira43 <n.rabone72@aol.com>
2025-09-05 04:16:46 +0100

    Created form using useForm

--- src/app/(private)/account/profile/page.tsx
+++ src/app/(private)/account/profile/page.tsx
@@ -9,9 +9,16 @@ import { Input } from '@/components/ui/input'
 
 function ProfilePage() {
   const formSchema = z.object({
-    usernaame: z.string().min(2, {
+    username: z.string().min(2, {
       message: 'Username must be at least 2 characters.',
     })
+  })
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+    },
   })
 
   return (
