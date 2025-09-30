9256f7728a9e51d6f575fcc9de8f08c0ef31194b
Nimira43 <n.rabone72@aol.com>
2025-09-30 22:18:12 +0100

    uploadFileAndGetUrl - 2

--- src/helpers/uploads.ts
+++ src/helpers/uploads.ts
@@ -1,1 +1,9 @@
-export const uploadFileAndGetUrl = async (file: File) => {}
+import supabase from '@/config/supabase-db-config'
+
+export const uploadFileAndGetUrl = async (file: File) => {
+  try {
+    const { data, error } = await supabase.storage.from('basic').upload(`${file.name}`, file) 
+  } catch (error: any) {
+    throw new Error(error)
+  }
+}
