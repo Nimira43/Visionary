040e17260c19d0c9b94fecc9aa9e759d9f56a95b
Nimira43 <n.rabone72@aol.com>
2025-10-10 22:07:32 +0100

    Reformatting of Add and Edit pages.

--- src/app/(private)/account/projects/add/page.tsx
+++ src/app/(private)/account/projects/add/page.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function AddProjectPage() {
   return (
-    <div>
-      <h1 className='text-xl font-medium'>Add Project</h1>
+    <div className='flex justify-center items-start min-h-screen p-4'>
+      <div className='w-full max-w-2xl'>
+        <h1 className='text-2xl text-main-dark font-medium mb-4 uppercase'>Add Project</h1>
+      </div>
     </div>
   )
 }
--- src/app/(private)/account/projects/edit/[id]/page.tsx
+++ src/app/(private)/account/projects/edit/[id]/page.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function EditProjectPage() {
   return (
-    <div>
-      <h1 className='text-xl font-medium'>Edit Project</h1>
+    <div className='flex justify-center items-start min-h-screen p-4'>
+      <div className='w-full max-w-2xl'>
+        <h1 className='text-2xl text-main-dark font-medium mb-4 uppercase'>Edit Project</h1>
+      </div>
     </div>
   )
 }
