0f03b3f71b17a6b6273f7ba2985135ddb9d002de
Nimira43 <n.rabone72@aol.com>
2025-10-10 21:53:52 +0100

    Add Project button

--- src/app/(private)/account/projects/page.tsx
+++ src/app/(private)/account/projects/page.tsx
@@ -1,10 +1,20 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 const ProjectsPage = () => {
   return (
-    <div className="flex justify-center items-start min-h-screen p-4">
-      <div className="w-full max-w-2xl">
-        <h1 className='text-2xl text-main-dark font-medium mb-4 uppercase text-center'>Projects</h1>
+    <div className='flex justify-center items-start min-h-screen p-4'>
+      <div className='w-full max-w-2xl'>
+        <div className='flex justify-between items-center'>
+          <h1 className='text-2xl text-main-dark font-medium mb-4 uppercase text-center'>Projects</h1>
+          <Button className='uppercase'>
+            <Link href='/account/projects/add'>
+              Add Project
+            </Link>
+          </Button>
+        </div>
+        
       </div>
     </div>
   )
