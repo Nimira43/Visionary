4497d5aeb219e9a8ac16e6d63674f36578151492
Nimira43 <n.rabone72@aol.com>
2025-10-10 22:26:25 +0100

    Passed in ProjectForm into Add and Edit pages

--- src/app/(private)/account/projects/add/page.tsx
+++ src/app/(private)/account/projects/add/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import ProjectForm from '../_components/project-form'
 
 function AddProjectPage() {
   return (
     <div className='flex justify-center items-start min-h-screen p-4'>
       <div className='w-full max-w-2xl'>
         <h1 className='text-2xl text-main-dark font-medium mb-4 uppercase'>Add Project</h1>
+        <ProjectForm formType='add' />
       </div>
     </div>
   )
--- src/app/(private)/account/projects/edit/[id]/page.tsx
+++ src/app/(private)/account/projects/edit/[id]/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import ProjectForm from '../../_components/project-form'
 
 function EditProjectPage() {
   return (
     <div className='flex justify-center items-start min-h-screen p-4'>
       <div className='w-full max-w-2xl'>
         <h1 className='text-2xl text-main-dark font-medium mb-4 uppercase'>Edit Project</h1>
+        <ProjectForm formType='edit' />
       </div>
     </div>
   )
