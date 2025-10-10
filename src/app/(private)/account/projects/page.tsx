2df32b2ddaf2ebeb2c8ffea421def13430e60d03
Nimira43 <n.rabone72@aol.com>
2025-10-10 21:41:05 +0100

    Setting up page layout and heading

--- src/app/(private)/account/projects/page.tsx
+++ src/app/(private)/account/projects/page.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const ProjectsPage = () => {
   return (
-    <div>
-      Projects Page
+    <div className="flex justify-center items-start min-h-screen p-4">
+      <div className="w-full max-w-2xl">
+        <h1 className='text-2xl text-main-dark font-medium mb-4 uppercase text-center'>Projects</h1>
+      </div>
     </div>
   )
 }
