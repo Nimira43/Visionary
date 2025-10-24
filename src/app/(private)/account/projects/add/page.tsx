4a6a53e4813400f3be4f89ec125b6510cbc44e12
Nimira43 <n.rabone72@aol.com>
2025-10-25 00:07:10 +0100

    Centering Add Project heading

--- src/app/(private)/account/projects/add/page.tsx
+++ src/app/(private)/account/projects/add/page.tsx
@@ -5,7 +5,7 @@ function AddProjectPage() {
   return (
     <div className='flex justify-center items-start min-h-screen p-4'>
       <div className='w-full max-w-2xl'>
-        <h1 className='text-2xl text-main-dark font-medium mb-4 uppercase'>Add Project</h1>
+        <h1 className='text-2xl text-main-dark font-medium mb-4 uppercase text-center'>Add Project</h1>
         <ProjectForm formType='add' />
       </div>
     </div>
