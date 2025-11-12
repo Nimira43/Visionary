b4ddfab5d00436f4546d847f6520b3a569063ed6
Nimira43 <n.rabone72@aol.com>
2025-11-12 20:07:28 +0000

    addNewProject - 3

--- src/actions/projects.ts
+++ src/actions/projects.ts
@@ -4,11 +4,19 @@ import supabase from '@/config/supabase-db-config'
 
 export const addNewProject = async (payload: any) => {
   try {
-    
+    const {data, error} = await supabase.from('projects').insert(payload)
+
+    if (error) throw new Error(error.message)
+
+    return {
+      success:  true,
+      data
+    }
+
   } catch (error: any) {
-    return (
+    return {
       success: false,
       error: error.message
-    )
+    }
   }
 }
