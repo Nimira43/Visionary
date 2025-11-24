1398a8964b5443c3d9a7fa72f369ffe7914f7be1
Nimira43 <n.rabone72@aol.com>
2025-11-24 03:28:44 +0000

    Basic Table layout

--- src/app/(private)/account/projects/_components/projects-table.tsx
+++ src/app/(private)/account/projects/_components/projects-table.tsx
@@ -1,12 +1,28 @@
 import { IProject } from '@/app/interfaces'
-import { Table } from '@/components/ui/table'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 
 function projectsTable({projects}: {
   projects: IProject[]
 }) {
   return (
     <Table>
-      
+      <TableCaption></TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead></TableHead>
+          <TableHead></TableHead>
+          <TableHead></TableHead>
+          <TableHead></TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell></TableCell>
+          <TableCell></TableCell>
+          <TableCell></TableCell>
+          <TableCell></TableCell>
+        </TableRow>
+      </TableBody>
     </Table>
   )
 }
