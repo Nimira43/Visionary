16c62c24cc1cd95f0690c5f01cd7f2f383394ba4
Nimira43 <n.rabone72@aol.com>
2025-12-09 08:21:26 +0000

    EditProjectPage - 2

--- src/app/(private)/account/projects/edit/[id]/page.tsx
+++ src/app/(private)/account/projects/edit/[id]/page.tsx
@@ -25,7 +25,10 @@ async function EditProjectPage({ params } : IEditProjectPageProps) {
     <div className='flex justify-center items-start min-h-screen p-4'>
       <div className='w-full max-w-2xl'>
         <h1 className='text-2xl text-main-dark font-medium mb-4 uppercase'>Edit Project</h1>
-        <ProjectForm formType='edit' />
+        <ProjectForm
+          formType='edit'
+          initialValues={projectResponse.data}
+        />
       </div>
     </div>
   )
