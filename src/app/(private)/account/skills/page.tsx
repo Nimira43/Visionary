e170e6809401c396ccb08ddde31031c42f839559
Nimira43 <n.rabone72@aol.com>
2026-03-22 02:24:28 +0000

    Displaying skills on the Skills page.

--- src/app/(private)/account/skills/page.tsx
+++ src/app/(private)/account/skills/page.tsx
@@ -1,11 +1,51 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import SkillForm from './_components/skill-form'
+import toast from 'react-hot-toast'
+import { getSkillsByUserId } from '@/actions/skills'
+import usersGlobalStore, { IUsersGlobalStore } from '@/global-store/users-store'
+import Spinner from '@/components/ui/spinner'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import { PiTrash } from 'react-icons/pi'
+import { AiOutlineEdit } from 'react-icons/ai'
+import { ISkill } from '@/app/interfaces'
 
 function SkillsPage() {
   const [openSkillForm, setOpenSkillForm] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [skills, setSkills] = useState([])
+  const { user } = usersGlobalStore() as IUsersGlobalStore
+
+  const fetchData = async () => {
+    try {
+      setLoading(true)
+      const response: any = await getSkillsByUserId(user?.id!)
+      if (!response.success) {
+        throw new Error(response.message)
+      }
+      setSkills(response.data)
+    } catch (error: any) {
+      toast.error(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const deleteSkillHandler = async (id: string) => {}
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const columns = [
+    '',
+    'Name',
+    'Level',
+    'Actions',
+  ]
+
   return (
     <div className='min-h-screen flex flex-col items-center p-4'>
       <div className='w-full max-w-5xl'>
@@ -20,6 +60,73 @@ function SkillsPage() {
             Add Skill
           </Button>
         </div>
+        {loading && (
+          <div className='flex justify-center items-center mt-40'>
+            <Spinner />
+          </div>
+        )}
+        {skills.length && (
+          <Table className='border border-grey-light'>
+            <TableHeader className='bg-grey-light-extra'>
+              <TableRow>
+                {columns.map((column, index) => (
+                  <TableHead 
+                    key={index}
+                    className='font-medium'
+                  >
+                    {column}
+                  </TableHead>
+                ))}
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {skills.map((skill: ISkill) => (
+                <TableRow
+                  key={skill.id}
+                >
+                  <TableCell>
+                    {skill.image && (
+                      <img
+                        src={skill.image}
+                        alt={skill.name}
+                        className='w-20 h-20 rounded'
+                      />
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    {skill.name}
+                  </TableCell>
+                  <TableCell>
+                    {skill.level}
+                  </TableCell>
+                  <TableCell>
+                    <div className='flex gap-5'>
+                      <Button
+                        variant='outline'
+                        size={'icon'}
+                        onClick={() => 
+                          deleteSkillHandler(skill.id)
+                        }
+                      >
+                        <PiTrash size={12} />
+                      </Button>
+                      <Button
+                        variant='outline'
+                        size={'icon'}
+                        onClick={() => {
+                          setOpenSkillForm(true)
+                        }}
+                      >
+                        <AiOutlineEdit size={12} />
+                      </Button>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
+
         {openSkillForm && (
           <SkillForm
             openSkillForm={openSkillForm}
