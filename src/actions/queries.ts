8a23e763df7223a950d6f0ac2c8ce5ade4f8d47a
Nimira43 <n.rabone72@aol.com>
2026-05-17 10:43:54 +0100

    Completed queries server action

---
+++ src/actions/queries.ts
@@ -0,0 +1,24 @@
+'use server'
+
+import supabase from '@/config/supabase-db-config'
+
+export const fetchQueriesOfUser = async (userId: string) => {
+  try {
+    const { data, error } = await supabase
+      .from('queries')
+      .select('*')
+      .eq('user.id', userId)
+    
+    if (error) throw new Error(error.message)
+    
+    return {
+      success: false,
+      messages: 'Queries not found.'
+    }
+  } catch (error: any) {
+    return {
+      success: false,
+      message: error.message
+    }
+  }
+} 
