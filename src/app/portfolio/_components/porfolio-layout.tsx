fed487bda7dec58458eee3061f685446dc98c533
Nimira43 <n.rabone72@aol.com>
2026-06-05 14:31:18 +0100

    fetchConfigurations

--- src/app/portfolio/_components/porfolio-layout.tsx
+++ src/app/portfolio/_components/porfolio-layout.tsx
@@ -1,15 +1,49 @@
 'use client'
 
+import { getConfiguration } from '@/actions/configurations'
+import Spinner from '@/components/ui/spinner'
 import { useParams, usePathname, useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
 
 function PortfolioLayout({
   children
 }: {
   children: React.ReactNode
-}) {
+  }) {
+  const [loading, setLoading] = useState(false)
+  
+  const [config, setConfig] = useState({
+    show_education: true,
+    show_percentage_in_education: true,
+    show_icons_in_skills: true,
+    show_levels_in_skills: true
+  })
+  
   const params = useParams()
   const pathname = usePathname()
   const router = useRouter()
+  const userId: any = params?.id
+
+  const fetchConfiguration = async () => {
+    try {
+      setLoading(true)
+      const response = await getConfiguration(userId)
+
+      if (response.success && response.data) {
+        setConfig(response.data)
+      }
+
+    } catch (error: any) {
+      toast.error(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchConfiguration()
+  }, [])
 
   let menuItems = [
     {
@@ -33,6 +67,18 @@ function PortfolioLayout({
       path: `/portfolio/${params.id}/contactus`
     },
   ]
+
+  if (config.show_education === false) {
+    menuItems = menuItems.filter((item) => item.name !== 'Education')
+  }
+
+  if (loading) {
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <Spinner />
+      </div>
+    )
+  }
 
   return (
     <div className='lg:px-24 px-5'>
