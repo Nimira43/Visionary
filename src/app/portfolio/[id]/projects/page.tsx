8e36db620b418a8b21757745a36c3db088162ce5
Nimira43 <n.rabone72@aol.com>
2026-07-02 22:23:06 +0100

    Starting work on ProjectsPage.

--- src/app/portfolio/[id]/projects/page.tsx
+++ src/app/portfolio/[id]/projects/page.tsx
@@ -1,4 +1,24 @@
-function ProjectsPage() {
+import { getProjectsByUserId } from "@/actions/projects"
+import { IProject } from "@/interfaces"
+
+interface ProjectsPageProps {
+  params: {
+    id: string
+  }
+}
+
+async function ProjectsPage({ params }: ProjectsPageProps) {
+  const { id } = await params
+  const projectsResponse: any = await getProjectsByUserId(id) 
+
+  if (!projectsResponse.success) {
+    return (
+      <div>Error: {projectsResponse.message}</div>
+    )
+  }
+
+  const projects: IProject[] = projectsResponse.data
+
   return (
     <div>
       ProjectsPage
